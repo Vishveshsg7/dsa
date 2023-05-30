531f77ba06738017313dbf71b2c5ee534f4a1aa8
Razor_2054 <103508415+Vishveshsg7@users.noreply.github.com>
2023-05-30 22:31:04 +0530

    Update grp C assign 13.cpp

--- grp C assign 13.cpp
+++ grp C assign 13.cpp
@@ -1,184 +1,76 @@
-
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-class graph
+int cost[10][10], i, j, k, n, qu[10], front, rear, v, visit[10], visited[10];
+int stk[10], top, visit1[10], visited1[10];
+
+int main()
 {
-   typedef struct node
-	{
-		int data;
-		struct node *next;
-	}Node;
-   Node *New,*temp,*list[10];
- public:
-   		graph();
-        void create();
-        void display();
-        
-};
- 
-graph::graph()
-{
- int i;
- for(i=0;i<10;i++)
-  {
-	  list[i]=new Node;
-      list[i]->data=0; 
-	  list[i]->next=NULL;
-  }
-}
-
-
-
-void graph::create()
-{
- int s,d;
- char ans;
- do
- {
-  cout<<"\n\tEnter the source vertices: ";
-  cin>>s;
-  cout<<"\n\tEnter the destination vertices: ";
-  cin>>d;
-  
-  New=new Node;
-  New->data=d;
-  New->next=NULL;
-  if(list[s]->next==NULL)
-	{
-	  list[s]->data=s;
-	  list[s]->next=New;
-     }
-  else
-   {
-     temp=list[s]->next;
-	while(temp->next!=NULL)
-     {
-       temp=temp->next;
-     }
-   temp->next=New;
-   }
-
-  New=new Node;
-  New->data=s;
-  New->next=NULL;
-  if(list[d]->next==NULL)
-	{
-	  list[d]->data=d;
-	  list[d]->next=New;
-     }  
-  else
-   {
-     temp=list[d]->next;
-	while(temp->next!=NULL)
-	  {
-		temp=temp->next;
-        }
-	temp->next=New;
-   }
-  cout<<"Do you want to continue: ";
-  cin>>ans;
- }while(ans=='y');
-
-}
-
-
-void graph::display()
-{
- int i;
- cout<<"\n\t**** Adj. List *****\n";
- for(i=1;list[i]->data!=0;i++)
-   {
-     cout<<"_";
-	cout<<"\n|"<<list[i]->data<<"| -> ";
-	for(temp=list[i]->next;temp!=NULL;temp=temp->next)
-       {
-		cout<<temp->data<<" ->";
-	  }
-	cout<<"NULL\n";
-     
-   }
-  cout<<" _\n";
-}
-   
- main()
-{
- int s;
- graph gr;
- int ch;
- char ans;
- do
-  {
-   cout<<"\n\t***** Adjacency List *****";
-   cout<<"\n\t1.Create\n\t2. Display\n\t3. Exit\n\t.....Enter Your Choice: ";
-   cin>>ch;
-    switch(ch)
-     {
-      case 1:
-             gr.create();
-             gr.display();
-			 break;
-      case 2:
-             
-             break;
-      case 3: 
- 			 break;
+    int m;
+    cout << "Enter number of vertices : ";
+    cin >> n;
+    cout << "Enter number of edges : ";
+    cin >> m;
+    
+    cout << "\nEDGES :\n";
+    for (k = 1; k <= m; k++)
+    {
+        cin >> i >> j;
+        cost[i][j] = 1;
+        cost[j][i] = 1;
     }
-   cout<<"Do you want to continue: ";
-   cin>>ans;      
-  }while(ans=='y');
+    //display function
+    cout << "The adjacency matrix of the graph is : " << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cout << " " << cost[i][j];
+        }
+        cout << endl;
+    }
+    cout << "Enter initial vertex : ";
+    cin >> v;
+    cout << "The BFS of the Graph is\n";
+    cout << v<<endl;
+    visited[v] = 1;
+    k = 1;
+    while (k < n)
+    {
+        for (j = 1; j <= n; j++)
+            if (cost[v][j] != 0 && visited[j] != 1 && visit[j] != 1)
+            {
+                visit[j] = 1;
+                qu[rear++] = j;
+            }
+        v = qu[front++];
+        cout << v << " ";
+        k++;
+        visit[v] = 0;
+        visited[v] = 1;
+    }
+    
+    cout <<endl<<"Enter initial vertex : ";
+    cin >> v;
+    cout << "The DFS of the Graph is\n";
+    cout << v<<endl;
+    visited[v] = 1;
+    k = 1;
+    while (k < n)
+    {
+        for (j = n; j >= 1; j--)
+            if (cost[v][j] != 0 && visited1[j] != 1 && visit1[j] != 1)
+            {
+                visit1[j] = 1;
+                stk[top] = j;
+                top++;
+            }
+        v = stk[--top];
+        cout << v << " ";
+        k++;
+        visit1[v] = 0;
+        visited1[v] = 1;
+    }
+    return 0;
 }
-
-
-/* op
-
-
-	***** Adjacency List *****
-	1.Create
-	2. Display
-	3. Exit
-	.....Enter Your Choice: 1
-
-	Enter the source vertices: 1
-
-	Enter the destination vertices: 2
-Do you want to continue: y
-
-	Enter the source vertices: 1
-
-	Enter the destination vertices: 3
-Do you want to continue: y
-
-	Enter the source vertices: 1
-
-	Enter the destination vertices: 4
-Do you want to continue: y
-
-	Enter the source vertices: 2
-
-	Enter the destination vertices: 4
-Do you want to continue: y
-
-	Enter the source vertices: 3
-
-	Enter the destination vertices: 4
-Do you want to continue: y
-
-	Enter the source vertices: 3
-
-	Enter the destination vertices: 5
-Do you want to continue: y
-
-	Enter the source vertices: 4
-
-	Enter the destination vertices: 5
-Do you want to continue: n
-
-	**** Adj. List *****
-
-|1| -> 2 ->3 ->4 ->NULL
-|2| -> 1 ->4 ->NULL
-|3| -> 1 ->4 ->5 ->NULL
-|4| -> 1 ->2 ->3 ->5 ->NULL
-|5| -> 3 ->4 ->NULLDo you want to continue: n 
-*/
